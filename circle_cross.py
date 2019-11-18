3d887e063ea6477f73f64234a2ae54350512d799
Pawel <pawel.pytel@op.pl>
2019-11-18 10:40:30 +0100

    dla niektórych przypadków obraca, trzeba dobrać rozmiary masek do rozmiaru obrazka, porobić odpowiednie zdjęcia

--- circle_cross.py
+++ circle_cross.py
@@ -2,184 +2,242 @@ import skimage as ski
 from matplotlib import pyplot as plt
 from skimage import data, io, filters, exposure
 from skimage.color import rgb2hsv, hsv2rgb, rgb2gray
-from skimage.feature import canny
-import warnings
 import skimage.morphology as mp
-from skimage import measure
 from skimage.transform import resize
 import numpy as np
+import photo_spliting as ps
+import math
 
-img = io.imread('images/photo01.jpg')
-result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-img = rgb2gray(img)
-img **= 3
-img[img >= 0.1] = 1
-for i in range(10):
-    img = mp.erosion(img)
-img = (img == 1) * 255
+
+def load_file(path):
+    return io.imread(path)
+
+
+def black_white(img):
+    img = rgb2gray(img)
+    img **= 3
+    img = (img <= 0.04) * 1
+    for i in range(1):
+        img = mp.dilation(img)
+    img = (img == 1) * 255
+    return img
 
 
 def cut(img, tag, color):
     if tag == 'allp':
-        while all(img[0, :] == color):
+        while (img[0, :] == color).all():
             img = np.delete(img, 0, 0)
-        while all(img[:, 0] == color):
+        while (img[:, 0] == color).all():
             img = np.delete(img, 0, 1)
-        while all(img[-1, :] == color):
+        while (img[-1, :] == color).all():
             img = np.delete(img, -1, 0)
-        while all(img[:, -1] == color):
+        while (img[:, -1] == color).all():
             img = np.delete(img, -1, 1)
     elif tag == 'anyp':
-        while any(img[0, :] == color):
+        while (img[0, :] == color).any():
             img = np.delete(img, 0, 0)
-        while any(img[:, 0] == color):
+        while (img[:, 0] == color).any():
             img = np.delete(img, 0, 1)
-        while any(img[-1, :] == color):
+        while (img[-1, :] == color).any():
             img = np.delete(img, -1, 0)
-        while any(img[:, -1] == color):
+        while (img[:, -1] == color).any():
             img = np.delete(img, -1, 1)
     return img
 
 
-img = cut(img, 'allp', 0)
-img = cut(img, 'anyp', 0)
-img = cut(img, 'allp', 255)
+def cut_min(img):
+    img = cut(img, 'allp', 255)
+    img = cut(img, 'anyp', 255)
+    img = cut(img, 'allp', 0)
+    return img
 
-spots = []
-spot2 = []
-square_range = 40
-for i in range(0, img.shape[0] - square_range):
-    for j in range(0, img.shape[1] - square_range):
-        square = np.array(img[i:i + square_range, j:j + square_range])
-        if (square[0, 0] == 255
-                and square[0, -1] == 255
-                and square[-1, 0] == 255
-                and square[-1, -1] == 255
-                and not all(square[0, :] == 255)
-                and not all(square[-1, :] == 255)
-                and not all(square[:, 0] == 255)
-                and not all(square[:, -1] == 255)):
-            spots.append([i + square_range // 2, j + square_range // 2])
+
+def find_board(img, square_range):
+    spots = []
+    for i in range(0, img.shape[0] - square_range):
+        for j in range(0, img.shape[1] - square_range):
+            square = np.array(img[i:i + square_range, j:j + square_range])
+            if (square[0, 0] == 0
+                    and square[0, -1] == 0
+                    and square[-1, 0] == 0
+                    and square[-1, -1] == 0
+                    and not all(square[0, :] == 0)
+                    and not all(square[-1, :] == 0)
+                    and not all(square[:, 0] == 0)
+                    and not all(square[:, -1] == 0)
+                    and square[square_range // 2, square_range // 2] == 255):
+                spots.append([i + square_range // 2, j + square_range // 2])
+    # for i in spots:
+    #     img[i[0], i[1]] = 125
+    top_right = []
+    top_left = []
+    bottom_right = []
+    bottom_left = []
+
+    for i in spots:
+        if i[0] < img.shape[0] // 2 and i[1] < img.shape[1] // 2:
+            top_left = i
+            break
+    for i in spots:
+        if i[0] > img.shape[0] // 2 and i[1] < img.shape[1] // 2:
+            bottom_left = i
+            break
+    for i in spots:
+        if i[0] < img.shape[0] // 2 and i[1] > img.shape[1] // 2:
+            top_right = i
+            break
+    for i in spots:
+        if i[0] > img.shape[0] // 2 and i[1] > img.shape[1] // 2:
+            bottom_right = i
+            break
+    return top_left, top_right, bottom_left, bottom_right
+
+
+def fill_board(img, top_right):
+    img = ski.segmentation.flood_fill(img, (top_right[0], top_right[1]), 125)
+    return img
+
+
+def find_crosses(img, square_range):
+    spot2 = []
+    for i in range(0, img.shape[0] - square_range):
+        for j in range(0, img.shape[1] - square_range):
+            square = np.array(img[i:i + square_range, j:j + square_range])
+            if (square[0, 0] == 255
+                    and square[0, -1] == 255
+                    and square[-1, 0] == 255
+                    and square[-1, -1] == 255
+                    and not all(square[0, :] == 255)
+                    and not all(square[-1, :] == 255)
+                    and not all(square[:, 0] == 255)
+                    and not all(square[:, -1] == 255)
+                    and square[square_range // 2, square_range // 2] == 255):
+                spot2.append([i + square_range // 2, j + square_range // 2])
+
+    for i in spot2:
+        img[i[0], i[1]] = 50
+    return img
+
+
+def find_result(img, top_left, top_right, bottom_left, bottom_right):
+    result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    if (img[0:top_left[0], 0:top_left[1]] == 50).any():
+        result[0][0] = 'X'
+    elif (img[0:top_left[0], 0:top_left[1]] == 255).any():
+        result[0][0] = 'O'
+    else:
+        result[0][0] = ' '
+    if (img[0:min(top_left[0], top_right[0]), top_left[1]:top_right[1]] == 50).any():
+        result[0][1] = 'X'
+    elif (img[0:min(top_left[0], top_right[0]), top_left[1]:top_right[1]] == 255).any():
+        result[0][1] = 'O'
+    else:
+        result[0][1] = ' '
+    if (img[:top_right[0], top_right[1]:] == 50).any():
+        result[0][2] = 'X'
+    elif (img[:top_right[0], top_right[1]:] == 255).any():
+        result[0][2] = 'O'
+    else:
+        result[0][2] = ' '
+    if (img[top_left[0]:bottom_left[0], 0:top_left[1]] == 50).any():
+        result[1][0] = 'X'
+    elif (img[top_left[0]:bottom_left[0], 0:top_left[1]] == 255).any():
+        result[1][0] = 'O'
+    else:
+        result[1][0] = ' '
+    if ((img[max(top_left[0], top_right[0]):min(bottom_left[0], bottom_right[0]),
+         max(top_left[1], bottom_left[1]):min(top_right[1], bottom_right[1])] == 50).any()):
+        result[1][1] = 'X'
+    elif ((img[max(top_left[0], top_right[0]):min(bottom_left[0], bottom_right[0]),
+           max(top_left[1], bottom_left[1]):min(top_right[1], bottom_right[1])] == 255).any()):
+        result[1][1] = 'O'
+    else:
+        result[1][1] = ' '
+    if (img[top_right[0]:bottom_right[0], max(bottom_right[1], top_right[1]):] == 50).any():
+        result[1][2] = 'X'
+    elif (img[top_right[0]:bottom_right[0], max(bottom_right[1], top_right[1]):] == 255).any():
+        result[1][2] = 'O'
+    else:
+        result[1][2] = ' '
+    if (img[bottom_left[0]:, 0:bottom_left[1]] == 50).any():
+        result[2][0] = 'X'
+    elif (img[bottom_left[0]:, 0:bottom_left[1]] == 255).any():
+        result[2][0] = 'O'
+    else:
+        result[2][0] = ' '
+    if (img[max(bottom_left[0], bottom_right[0]):, bottom_left[1]:bottom_right[1]] == 50).any():
+        result[2][1] = 'X'
+    elif (img[max(bottom_left[0], bottom_right[0]):, bottom_left[1]:bottom_right[1]] == 255).any():
+        result[2][1] = 'O'
+    else:
+        result[2][1] = ' '
+    if (img[bottom_right[0]:, bottom_right[1]:] == 50).any():
+        result[2][2] = 'X'
+    elif (img[bottom_right[0]:, bottom_right[1]:] == 255).any():
+        result[2][2] = 'O'
+    else:
+        result[2][2] = ' '
+    return result
+
+
+def print_result(result):
+    for i in result:
+        print(''.join(i))
+
+
+def show_img(img):
+    plt.grid(True)
+    io.imshow(img, cmap=plt.cm.gray)
+    io.show()
+
+
+def rotate(rot):
+    corners = ski.transform.probabilistic_hough_line(rot)
+    line = corners[0]
+    rot[line[1][1], line[1][0]] = 125
+    c = np.array([line[0][0], line[1][1]])
+    len1 = np.linalg.norm(c - np.array(line[0]))
+    len2 = np.linalg.norm(c - np.array(line[1]))
+
+    if line[0][0] >= line[1][0] and line[0][1] >= line[1][0]:
+        if len2 != 0:
+            tangens = len1 / len2
+        else:
+            tangens = 0
+    else:
+        if len1 != 0:
+            tangens = len2 / len1
+        else:
+            tangens = 0
+
+    angle = np.degrees(math.atan(tangens))
+    if angle > 10:
+        rot = ps.make_big_square(rot)
+        rot = ski.transform.rotate(rot, angle)
+        rot = cut_min(rot)
+        rot = (rot >= 1) * 255
+        rot = mp.dilation(mp.erosion(rot))
+    return rot
+
 
 top_right = []
 top_left = []
 bottom_right = []
 bottom_left = []
+images = []
+result = []
+img = load_file('images/photo02.jpg')
+img = black_white(img)
+img = cut_min(img)
+images = ps.photo_division(img)
+for i in images:
+    i = rotate(i)
 
-for i in spots:
-    if i[0] < img.shape[0] // 2 and i[1] < img.shape[1] // 2:
-        top_left = i
-        break
-for i in spots:
-    if i[0] > img.shape[0] // 2 and i[1] < img.shape[1] // 2:
-        bottom_left = i
-        break
-for i in spots:
-    if i[0] < img.shape[0] // 2 and i[1] > img.shape[1] // 2:
-        top_right = i
-        break
-for i in spots:
-    if i[0] > img.shape[0] // 2 and i[1] > img.shape[1] // 2:
-        bottom_right = i
-        break
-# spot_sums=[sum(i) for i in spots]
-# min_sum=min(spot_sums)
-# max_sum=max(spot_sums)
-# for i,j in zip(spot_sums,spots):
-#     if i ==  min_sum:
-#         top_left=j
-#     if i == max_sum:
-#         top_right=j
-# for i in spots:
-#     for j in [-1,0,1]:
-#         for k in [-1,0,1]:
-#             if not (k==j and j==0):
-#                 if i not in to_remove:
-#                     if [i[0]+k, i[1] + j] in spots:
-#                         #img[i[0]+k, i[1] + j] = 0
-#                         to_remove.append([i[0]+k, i[1] + j])
-# for i in to_remove:
-#     try:
-#         if i in spots:
-#             spots.remove(i)
-#     except ValueError:
-#         print(i)
-img = ski.segmentation.flood_fill(img, (top_right[0], top_right[1]), 125)
-for i in range(0, img.shape[0] - square_range):
-    for j in range(0, img.shape[1] - square_range):
-        square = np.array(img[i:i + square_range, j:j + square_range])
-        if (square[0, 0] == 0
-                and square[0, -1] == 0
-                and square[-1, 0] == 0
-                and square[-1, -1] == 0
-                and not all(square[0, :] == 0)
-                and not all(square[-1, :] == 0)
-                and not all(square[:, 0] == 0)
-                and not all(square[:, -1] == 0)):
-            spot2.append([i + square_range // 2, j + square_range // 2])
-for i in spot2:
-    img[i[0], i[1]] = 50
-print(top_left,top_right)
-print(bottom_left,bottom_right)
-print((img[:top_left[0], :top_left[1]]==50).all())
-if (img[ 0:top_left[0],0:top_left[1]] == 50).any():
-    result[0][0] = 'X'
-elif (img[0:top_left[0], 0:top_left[1]] == 0).any():
-    result[0][0] = 'O'
-else:
-    result[0][0] = ' '
-if (img[ 0:min(top_left[0], top_right[0]),top_left[1]:top_right[1]] == 50).any():
-    result[0][1] = 'X'
-elif (img[ 0:min(top_left[0], top_right[0]),top_left[1]:top_right[1]] == 0).any():
-    result[0][1] = 'O'
-else:
-    result[0][1] = ' '
-if (img[ :top_right[0],top_right[1]:] == 50).any():
-    result[0][2] = 'X'
-elif (img[ :top_right[0],top_right[1]:] == 0).any():
-    result[0][2] = 'O'
-else:
-    result[0][2] = ' '
-if (img[ top_left[0]:bottom_left[0],0:top_left[1]] == 50).any():
-    result[1][0] = 'X'
-elif (img[ top_left[0]:bottom_left[0],0:top_left[1]] == 0).any():
-    result[1][0] = 'O'
-else:
-    result[1][0] = ' '
-if ((img[max(top_left[0], top_right[0]):min(bottom_left[0], bottom_right[0]),
-     max(top_left[1], bottom_left[1]):min(top_right[1], bottom_right[1])] == 50).any()):
-    result[1][1] = 'X'
-elif ((img[max(top_left[0], top_right[0]):min(bottom_left[0], bottom_right[0]),
-     max(top_left[1], bottom_left[1]):min(top_right[1], bottom_right[1])] == 0).any()):
-    result[1][1] = 'O'
-else:
-    result[1][1] = ' '
-if (img[ top_right[0]:bottom_right[0],max(bottom_right[1],top_right[1]):] == 50).any():
-    result[1][2] = 'X'
-elif (img[ top_right[0]:bottom_right[0],max(bottom_right[1],top_right[1]):] == 0).any():
-    result[1][2] = 'O'
-else:
-    result[1][2] = ' '
-if (img[ bottom_left[0]:,0:bottom_left[1]] == 50).any():
-    result[2][0] = 'X'
-elif (img[ bottom_left[0]:,0:bottom_left[1]] == 0).any():
-    result[2][0] = 'O'
-else:
-    result[2][0] = ' '
-if (img[max(bottom_left[0],bottom_right[0]):, bottom_left[1]:bottom_right[1]] == 50).any():
-    result[2][1] = 'X'
-elif (img[max(bottom_left[0],bottom_right[0]):, bottom_left[1]:bottom_right[1]] == 0).any():
-    result[2][1] = 'O'
-else:
-    result[2][1] = ' '
-if (img[ bottom_right[0]:,bottom_right[1]:] == 50).any():
-    result[2][2] = 'X'
-elif (img[ bottom_right[0]:,bottom_right[1]:] == 0).any():
-    result[2][2] = 'O'
-else:
-    result[2][2] = ' '
-for i in result:
-    print(''.join(i))
-plt.grid(True)
-io.imshow(img, cmap=plt.cm.gray)
-io.show()
+    top_left, top_right, bottom_left, bottom_right = find_board(i, 20)
+    i = find_crosses(i, 10)
+
+    i = fill_board(i, top_right)
+    show_img(i)
+    print(top_left, top_right, bottom_left, bottom_right)
+    result = find_result(i, top_left, top_right, bottom_left, bottom_right)
+    print_result(result)
--- photo_spliting.py
+++ photo_spliting.py
@@ -7,6 +7,7 @@ from matplotlib import pylab as plt, gridspec
 import numpy as np
 from ipykernel.pylab.backend_inline import flush_figures
 import warnings
+import math
 
 
 def thresh(t, photo):
@@ -39,6 +40,10 @@ def cut(img, tag, color):
     return img
 
 
+def big2small(img):
+    return img / 255
+
+
 def find_white_line(photo):
     photo = big2small(photo)
     max_value = 0
@@ -48,45 +53,39 @@ def find_white_line(photo):
     return max_value / len(photo[0])
 
 
-def photo_div(photo):
-    for p in range(len(photo)):
-        if 0 not in photo[p]:
-            new_img1 = photo[:p, :]
-            new_img2 = photo[p:, :]
-            new_img1 = cut(new_img1, 'allp', 255)
-            new_img2 = cut(new_img2, 'allp', 255)
-            photo_div(new_img1)
-            photo_div(new_img2)
-            break
-    else:
-        iloczyn = 1
-        for p in range(len(photo[0])):
-            for q in range(len(photo)):
-                iloczyn *= photo[q][p]
-            if iloczyn != 0:
-                photo1 = photo[:, :p]
-                photo2 = photo[:, p:]
-                photo1 = cut(photo1, 'allp', 255)
-                photo2 = cut(photo2, 'allp', 255)
-                photo_div(photo1)
-                photo_div(photo2)
+def photo_division(img):
+    final_list = []
+
+    def photo_div(photo):
+        for p in range(len(photo)):
+            if 255 not in photo[p]:
+                new_img1 = photo[:p, :]
+                new_img2 = photo[p:, :]
+                new_img1 = cut(new_img1, 'allp', 0)
+                new_img2 = cut(new_img2, 'allp', 0)
+                photo_div(new_img1)
+                photo_div(new_img2)
                 break
-            iloczyn = 1
         else:
-            final_list.append(photo)
+            iloczyn = 1
+            for p in range(len(photo[0])):
+                for q in range(len(photo)):
+                    iloczyn *= (photo[q][p] == 0)
+                if iloczyn != 0:
+                    photo1 = photo[:, :p]
+                    photo2 = photo[:, p:]
+                    photo1 = cut(photo1, 'allp', 0)
+                    photo2 = cut(photo2, 'allp', 0)
+                    photo_div(photo1)
+                    photo_div(photo2)
+                    break
+                iloczyn = 1
+            else:
+                final_list.append(photo)
 
-
-final_list = []
-photo_div(x)
-print(len(final_list))
-
-
-for a in final_list:
-    io.imshow(a, cmap=plt.cm.gray)
-    io.show()
-
-
-
+    photo_div(img)
+    print(len(final_list))
+    return final_list
 
 
 def to_square(photo):
@@ -105,7 +104,7 @@ def find_line(photo_square, angle):
         for y in range(size):
             for x in range(size):
                 if x != k:
-                    if tangens == round((y-k)/(x-k), 2) and photo_square[y][x] == 0:
+                    if tangens == round((y - k) / (x - k), 2) and photo_square[y][x] == 0:
                         suma += 1
         if suma > max_val:
             max_val = suma
@@ -113,19 +112,23 @@ def find_line(photo_square, angle):
 
 
 def rotate(photo):
-    for i in range(360):
+    for i in range(90):
         photo = skimage.transform.rotate(photo, 1)
-        # photo = thresh(0.5, photo)
+        photo = (photo != 0) * 1
         if find_white_line(photo) > 0.65:
             break
-        io.imshow(photo, cmap=plt.cm.gray)
-        io.show()
     return photo
+
 
 # ta funckcja musi być, bo przy obracaniu obcinają się boki i dzięki powiększeniu obrazu plansza zostaje w całości
 def make_big_square(photo):
     max_size = max(photo.shape)
-    new_photo = 1 - np.zeros((int(1.4 * max_size) + 1, int(1.4 * max_size) + 1))
+    new_photo = np.zeros((int(1.4 * max_size) + 1, int(1.4 * max_size) + 1))
     sp = int(0.2 * max_size)
-    new_photo[sp:sp+photo.shape[0], sp:sp+photo.shape[1]] = photo
+    new_photo[sp:sp + photo.shape[0], sp:sp + photo.shape[1]] = photo
     return new_photo
+
+# img=cc.load_file('images/photo01.jpg')
+# img=cc.cut_min(img)
+# img=cc.black_white(img)
+# cc.show_img(img)
